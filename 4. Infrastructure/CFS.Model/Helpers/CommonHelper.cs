ff6bebb5ad793e49db3f8363edd9071a2f283bca
sw-ms-manojahuja <64135665+sw-ms-manojahuja@users.noreply.github.com>
2022-02-01 15:07:47 +0530

    first commit

---
+++ 4. Infrastructure/CFS.Model/Helpers/CommonHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFS.Model.Helpers
+{
+    public static class CommonHelper
+    {
+    }
+}
